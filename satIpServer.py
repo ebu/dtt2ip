89bdd17a983a12dad36ae9eb0c1788cb1ef156f8
alexmnt <Alex@Alexandrus-MacBook-Pro.local>
2015-07-09 13:35:46 +0200

    Server Advertisments

--- satIpServer.py
+++ satIpServer.py
@@ -10,6 +10,16 @@ from twisted.internet.protocol import DatagramProtocol
 from netInterfaceStatus import getServerIP
 import re
 import os
+from threading import Timer
+
+global t
+global datagramNotify
+global deviceIdOk
+global deviceID
+
+deviceID = 1
+datagramNotify = ''
+deviceIdOk = False
 
 ssdpAddr = '239.255.255.250'
 ssdpPort = 1900
@@ -18,45 +28,70 @@ httpPort = 80
 
 serverIP = getServerIP()
 
-MSOK = 'HTTP/1.1 200 OK\r\nCACHE-CONTROL:max-age=1800\r\nDATE:Mon 03 Jan 2000 11:29:31 GMT\r\nEXT:\r\nLOCATION:http://%s:%d/desc.xml\r\nSERVER:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nST:urn:ses-com:device:SatIPServer:1\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd::urn:ses-com:device:SatIPServer:1\r\nBOOTID.UPNP.ORG:998\r\nCONFIGID.UPNP.ORG:2212703\r\nDEVICEID.SES.COM:0\r\n\r\n' % (serverIP, httpPort)
+MS_OK = 'HTTP/1.1 200 OK\r\nCACHE-CONTROL:max-age=1800\r\nDATE:Mon 03 Jan 2000 11:29:31 GMT\r\nEXT:\r\nLOCATION:http://%s:%d/desc.xml\r\nSERVER:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nST:urn:ses-com:device:SatIPServer:1\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd::urn:ses-com:device:SatIPServer:1\r\nBOOTID.UPNP.ORG:998\r\nCONFIGID.UPNP.ORG:2212703\r\nDEVICEID.SES.COM:%d\r\n\r\n' % (serverIP, httpPort, deviceID)
 
+MS_NOTIFY_ALIVE = 'NOTIFY * HTTP/1.1\r\nHOST: %s:%d\r\nBOOTID.UPNP.ORG:981\r\nCONFIGID.UPNP.ORG:12555367\r\nCACHE-CONTROL: max-age=1800\r\nLOCATION:http://%s/desc.xml\r\nNT:upnp:rootdevice\r\nNTS:ssdp:alive\r\nSERVER:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd::upnp:rootdevice\r\nDEVICEID.SES.COM:%d\r\n\r\n' % (ssdpAddr, ssdpPort, serverIP, deviceID)
 
-# SSDP_ADDR = '239.255.255.250'
-# # SSDP_ADDR = '192.168.1.38'
-# # SSDP_ADDR2 = '192.168.1.101'
-# SSDP_PORT = 1900
-# HTTP_PORT = 80
-# SERVER_IP = getServerIP()
- 
-# MS = 'M-SEARCH * HTTP/1.1\r\nHOST: %s:%d\r\nMAN: "ssdp:discover"\r\nMX: 2\r\nST: ssdp:all\r\n\r\n' % (SSDP_ADDR, SSDP_PORT)
-# NO =[]
-# a = 'NOTIFY * HTTP/1.1\r\nHOST: %s:%d\r\nBOOTID.UPNP.ORG:981\r\nCONFIGID.UPNP.ORG:12555367\r\nCACHE-CONTROL: max-age=1800\r\nLOCATION:http://192.168.1.61/desc.xml\r\nNT:upnp:rootdevice\r\nNTS:ssdp:alive\r\nSERVER:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd::upnp:rootdevice\r\nDEVICEID.SES.COM:0\r\n\r\n' % (SSDP_ADDR, SSDP_PORT)
-# b = 'NOTIFY * HTTP/1.1\r\nHOST: %s:%d\r\nBOOTID.UPNP.ORG:981\r\nCONFIGID.UPNP.ORG:12555367\r\nCACHE-CONTROL: max-age=1800\r\nLOCATION:http://192.168.1.61/desc.xml\r\nNT:uuid:7f764937-d0d7-11e1-9b23-00059e9651fd\r\nNTS:ssdp:alive\r\nSERVER:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd\r\nDEVICEID.SES.COM:0\r\n\r\n' % (SSDP_ADDR, SSDP_PORT)
-# c = 'NOTIFY * HTTP/1.1\r\nHOST: %s:%d\r\nBOOTID.UPNP.ORG:981\r\nCONFIGID.UPNP.ORG:12555367\r\nCACHE-CONTROL: max-age=1800\r\nLOCATION:http://192.168.1.61/desc.xml\r\nNT:urn:ses-com:device:SatIPServer:1\r\nNTS:ssdp:alive\r\nSERVER:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd::urn:ses-com:device:SatIPServer:1\r\nDEVICEID.SES.COM:0\r\n\r\n' % (SSDP_ADDR, SSDP_PORT)
-# MSOK = 'HTTP/1.1 200 OK\r\nCACHE-CONTROL:max-age=1800\r\nDATE:Mon 03 Jan 2000 11:29:31 GMT\r\nEXT:\r\nLOCATION:http://%s:%d/desc.xml\r\nSERVER:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nST:urn:ses-com:device:SatIPServer:1\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd::urn:ses-com:device:SatIPServer:1\r\nBOOTID.UPNP.ORG:998\r\nCONFIGID.UPNP.ORG:2212703\r\nDEVICEID.SES.COM:0\r\n\r\n' % (SERVER_IP, HTTP_PORT)
-# NO.append(a)
-# NO.append(b)
-# NO.append(c)
-# counter = 0
+MS_NOTIFY_BYEBYE = 'NOTIFY * HTTP/1.1\r\nHOST: %s:%d\r\nNT:upnp:rootdevice\r\nNTS:ssdp:byebye\r\nUSN:uuip:7f764937-d0d7-11e1-9b23-00059e9651fd::upnp:rootdevice\r\nBOOTID.UPNP.ORG:981\r\nCONFIGID.UPNP.ORG:12555367\r\n\r\n' % (ssdpAddr, ssdpPort)
+
+MS_SEARCH = 'M-SEARCH * HTTP/1.1\r\nHOST:%s:%d\r\nMAN:"ssdp-discover"ST:urn:ses-com:device:SatIPServer:1\r\nUSER-AGENT:CW/1.0 UPnP/1.1 CWUpnp/1.1\r\nDEVICEID.SES.COM:%d' % (serverIP, ssdpPort, deviceID)
+
+def timeout():
+	deviceIdOk = True
+
 
 class Base(DatagramProtocol):
+
+	def sendNOTIFY(self, msNOTIFY):
+		global t
+		for i in range(3):
+			self.ssdp.write(msNOTIFY, (ssdpAddr, ssdpPort))
+			print "msNOTIFY", msNOTIFY
+		if msNOTIFY == MS_NOTIFY_ALIVE:
+			t.start()
+
 	def datagramReceived(self, datagram, address):
+		global datagramNotify
+		global deviceIdOk
+		global deviceID
+
 		datagram_array = datagram.rsplit('\r\n')
+		datagramNotify = datagram_array
 		# print "datagram_array", datagram_array
 		# first_line = datagram.rsplit('\r\n')[0]
-
+		
 		try: 
 			first_line = datagram_array[0]
-			second_line = datagram_array[2]
+			# second_line = datagram_array[2]
 
-			match = re.search(r'M-SEARCH',first_line)
-			if match:
+			matchNotify = re.search(r'NOTIFY',first_line)
+			matchMSearch = re.search(r'M-SEARCH',first_line)
+
+			if matchNotify:
+				matchSES = re.search(r'DEVICEID.SES.COM:([\w]+)',datagram)
+				if matchSES:
+					if matchSES.group(1) == deviceID:
+						self.ssdp.write(MS_OK, (address[0], address[1]))
+						print "MS_OK"
+
+
+
+			if matchMSearch:
 				match2 = re.search(r'ssdp:discover',datagram)
-				if match2:
+				match3 = re.search(r'DEVICEID.SES.COM',datagram)
+				
+				if match3 and not deviceIdOk:
+					t.cancel()
+					self.ssdp.write(MS_OK, (address[0], address[1]))
+					self.sendNOTIFY(MS_NOTIFY_BYEBYE)
+					deviceID = deviceID + 1
+					self.sendNOTIFY(MS_NOTIFY_ALIVE)
+
+				elif match2:
 					# print 'received M-SEARCH from SAT>IP clients'
 					# print 'sending replay'
 					print 'ip', address[0], 'port', address[1]
-					self.ssdp.write(MSOK, (address[0], address[1]))
+					self.ssdp.write(MS_OK, (address[0], address[1]))
 
 					# Run server RTSP
 					# os.spawnl(os.P_DETACH, 'python Server.py 8005')
@@ -69,14 +104,22 @@ class Base(DatagramProtocol):
 		pass
  
 class Server(Base):
+
 	def __init__(self, iface, ssdpPort, ssdpAddr):
+		global t
+		t = Timer(5, timeout)
+
 		self.iface = iface
 		self.ssdp = reactor.listenMulticast(ssdpPort, self, listenMultiple=True)
 		self.ssdp.setLoopbackMode(1)
 		self.ssdp.joinGroup(ssdpAddr, interface=iface)
+
+		# self.ssdpClient = reactor.listenUDP(1900, self, interface=self.iface)
+		self.sendNOTIFY(MS_NOTIFY_ALIVE)
+
  
 	def stop(self):
-		self.ssdp.leaveGroup(ssdpPAddr, interface=self.iface)
+		self.ssdp.leaveGroup(ssdpAddr, interface=self.iface)
 		self.ssdp.stopListening()
  
 def main():
@@ -85,13 +128,6 @@ def main():
 	reactor.addSystemEventTrigger('before', 'shutdown', obj.stop)
  
 if __name__ == "__main__":
-	#testing the getNetworkInt() function
-	# a = getNetworkInt()
-	# print a
-	# if len(sys.argv) != 3:
-	# 	print "Usage: %s <server> <IP of interface>" % (sys.argv[0], )
-	# 	sys.exit(1)
-	# mode, iface = sys.argv[1:]
 	reactor.callWhenRunning(main)
 	reactor.run()
 
