13126f71c49f1ccfcea786d598900cb93b30fb33
Alex <alexandru_munteanu_7@yahoo.com>
2016-02-16 09:33:37 +0100

    http

--- nas3.sh
+++ nas3.sh
@@ -4,12 +4,10 @@
 echo "Killing NAS Discovery server..."
 #pids=`ps -aux | grep -wF 1900 | grep -v grep | awk '{print $2}'`
 pids=`netstat -lptu | grep 1900 | grep -v grep | awk '{print $6}' | cut -d / -f 1 `
-for i in "${pids[@]}"
-do
-  :
-  echo "Killing pid= ${pids[{$i}]}"
-   # kill -9 ${pids[$i]}
-done
+k=0
+for ((l=${pids[0]};k<${#pids[@]} && l<=${pids[k]};l++)); do
+	((++k))
+	echo pids[l]
 
 # Update and upgrade
 echo "Update the packages..."
