4921ded1c990b314c5ee6eec2faf8c5f9f3a3ac9
Alex <alexandru_munteanu_7@yahoo.com>
2016-02-16 06:50:47 +0100

    http installing script

---
+++ http/nas.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Instalation Directory
+LOCATION=`pwd` 
+echo $LOCATION
+
+# Replace start-stop-status script with the one provided
+echo "Copy the new start-stop-status script..."
+# cp start-stop-status /var/packages/debian-chroot/scripts/start-stop-status
+cp start-stop-status start-stop-status3
+
+# Chroot into debian
+echo "Chroot to debian..."
+# /var/packages/debian-chroot/scripts/start-stop-status chroot $LOCATION
+./start-stop-status3 chroot $LOCATION
+
+# Kill the Discovery server from NAS
+echo "Killing NAS Discovery server..."
+pids=`ps -aux | grep -wF 1900 | grep -v grep | awk '{print $2}'`
+for i in "${pids[@]}"
+do
+   :
+   echo "Killing pid= ${pids[$i]}"
+   kill -9 ${pids[$i]}
+done
+
+# Update and upgrade
+echo "Update the packages..."
+# apt-get update
+# apt-get -y --force-yes upgrade
+
+# Install mumudvb dependencie
+echo "Install mumudvb dependencie..."
+# apt-get install git devscripts pgpgpg debhelper 
+
+# Clone mumudvb
+echo "Clone mumudvb..."
+git clone git://github.com/braice/MuMuDVB.git mumudvb
+
+# Changing directory
+echo "Changing directory..."
+cd mumudvb
+
+# Installing mumudvb
+echo "Installing mumudvb...autoreconf"
+autoreconf -i -f
+echo "Configure..."
+./configure
+echo "Make..."
+make
+echo "Make install..."
+make install
+
+# Other
+echo "Other"
+
