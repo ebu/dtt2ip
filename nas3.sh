f8fd392b6e6577b5a064d4c7a00e8cf76a10e718
Alex <alexandru_munteanu_7@yahoo.com>
2016-02-16 09:42:02 +0100

    http

--- nas3.sh
+++ nas3.sh
@@ -1,41 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 
 # Kill the Discovery server from NAS
-echo "Killing NAS Discovery server..."
-#pids=`ps -aux | grep -wF 1900 | grep -v grep | awk '{print $2}'`
-pids=`netstat -lptu | grep 1900 | grep -v grep | awk '{print $6}' | cut -d / -f 1 `
-k=0
-for ((l=${pids[0]};k<${#pids[@]} && l<=${pids[k]};l++)); do
-	((++k))
-	echo pids[l]
+echo "Killing NAS Discovery server...for later(Python)"
+# #pids=`ps -aux | grep -wF 1900 | grep -v grep | awk '{print $2}'`
+# pids=`netstat -lptu | grep 1900 | grep -v grep | awk '{print $6}' | cut -d / -f 1 `
+# k=0
+# for ((l=${pids[0]};k<${#pids[@]} && l<=${pids[k]};l++)); do
+# 	((++k))
+# 	echo pids[l]
 
 # Update and upgrade
 echo "Update the packages..."
-# apt-get update
-# apt-get -y --force-yes upgrade
+apt-get update
+apt-get -y --force-yes upgrade
 
-# Install git
 # Install mumudvb dependencie
 echo "Install mumudvb dependencie..."
-# apt-get install git devscripts pgpgpg debhelper 
+apt-get install git devscripts pgpgpg debhelper 
+
+# Change directory to the one selected
+echo "Chaning directory..."
 
 # Clone mumudvb
 echo "Clone mumudvb..."
-#git clone git://github.com/braice/MuMuDVB.git mumudvb
+git clone git://github.com/braice/MuMuDVB.git mumudvb
 
 # Changing directory
 echo "Changing directory..."
-# cd mumudvb
+cd mumudvb
 
 # Installing mumudvb
 echo "Installing mumudvb...autoreconf"
-# autoreconf -i -f
+autoreconf -i -f
 echo "Configure..."
-# ./configure
+./configure
 echo "Make..."
-# make
+make
 echo "Make install..."
-# make install
-
-# Other
-echo "Other"
+make install
