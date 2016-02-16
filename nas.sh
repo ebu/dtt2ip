681df9b987ead8bf834eef9bb32803f009d234f5
Alex <alexandru_munteanu_7@yahoo.com>
2016-02-16 08:46:14 +0100

    http

--- nas.sh
+++ nas.sh
@@ -2,5 +2,5 @@
 
 # Replace start-stop-status script with the one provided
 echo "Copy the new start-stop-status script..."
-cp http/start-stop-status /var/packages/debian-chroot/scripts/start-stop-status
+cp start-stop-status /var/packages/debian-chroot/scripts/start-stop-status
 #cp start-stop-status start-stop-status3
